554cda0d02805b985ef33ef880659451952d20bb
Andrea Leopardi <an.leopardi@gmail.com>
2015-07-05 13:00:34 +0200

    Add a wh/create-bash-script interactive function
    It's used to create bash scripts in `~/bin`.

--- init.el
+++ init.el
@@ -21,6 +21,20 @@
     (up-list)
     (eval-last-sexp nil)))
 
+(defun wh/create-bash-script (name)
+  "Create a bash script in ~/bin.
+
+The script will be called `NAME'. A bash shebang will be inserted on the first
+line and the script will be made executable for the user."
+  (interactive "sName: ")
+  (let ((path (concat "~/bin/" name)))
+    (find-file path)
+    (insert "#!/bin/bash\n\n\n")
+    (end-of-buffer)
+    (save-buffer)
+    (shell-script-mode)
+    (shell-command (format "chmod u+x %s" path))))
+
 (global-set-key (kbd "<f2>") 'wh/edit-init-file)
 (global-set-key (kbd "C-k") 'kill-whole-line)
 
