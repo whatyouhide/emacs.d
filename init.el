f4ce0067b6d0fb4977a38192302b1cb67f52da45
Andrea Leopardi <an.leopardi@gmail.com>
2015-07-21 09:40:47 +0200

    Start GUI Emacs with a maximized frame

--- init.el
+++ init.el
@@ -356,6 +356,9 @@
 ;; Font size in 1/10pt (140 = 14 pt).
 (set-face-attribute 'default nil :height 140)
 
+(when (display-graphic-p)
+  (toggle-frame-maximized))
+
 ;; Left Option is Meta, right Option doesn't do anything in Emacs (so it can be
 ;; used for accented letters and such).
 (setq mac-option-key-is-meta t)
