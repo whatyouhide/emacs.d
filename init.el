2cee36f52bcdf2abb799ceec95c8d0b5fe47b936
Andrea Leopardi <an.leopardi@gmail.com>
2015-07-25 18:48:04 +0200

    Move custom-related stuff into etc/custom.el

--- init.el
+++ init.el
@@ -380,20 +380,5 @@
 (setq mac-option-key-is-meta t)
 (setq mac-right-option-modifier nil)
 
-;; ;; Start with a maximized window.
-;; (toggle-frame-fullscreen)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" default)))
- '(magit-use-overlays nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file "~/.emacs.d/etc/custom.el")
+(load custom-file)
