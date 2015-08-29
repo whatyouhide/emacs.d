5ac7c5ddf1e020529a62b924f49f743b4bd72d7d
Andrea Leopardi <an.leopardi@gmail.com>
2015-08-29 18:57:22 +0200

    Dump Cask and just use use-package
    Wow, it was pretty exhausting!

--- init.el
+++ init.el
@@ -1,27 +1,51 @@
-(require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
-(cask-initialize)
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/"))
 
-(require 'use-package)
+(setq use-package-verbose t)
+
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "ext" user-emacs-directory))
 
+;; Make use-package available.
+(require 'use-package)
+
 ;; Functions that will be used also throughout this file.
 (use-package wh-functions)
 
+
+;; Theming
+
+(use-package aurora-theme :ensure t)
+(use-package color-theme-sanityinc-tomorrow :ensure t)
+(use-package gruvbox-theme :ensure t)
+(use-package material-theme :ensure t)
+(use-package molokai-theme :ensure t)
+(use-package monokai-theme :ensure t)
+(use-package zenburn-theme :ensure t)
+
 (setq wh/nice-gui-themes
-      '(zenburn
-        gruvbox
-        leuven
-        material
-        monokai
-        molokai
-        sanityinc-tomorrow-day
-        wombat))
+      '(gruvbox
+	leuven
+	material
+	monokai
+	molokai
+	sanityinc-tomorrow-day
+	wombat
+	zenburn))
 
 (setq wh/nice-term-theme 'monokai)
-
 (wh/load-random-gui-theme)
+
+;; Global keyboarding
 
 (global-set-key (kbd "<f8>") 'wh/edit-init-file)
 (global-set-key (kbd "<f9>") 'wh/edit-notes-file)
@@ -31,7 +55,6 @@
 ;; Always as "y or n", not that annoying "yes or no".
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-(add-to-list 'auto-mode-alist '("Cask\\'" . emacs-lisp-mode))
 
 ;; Initial buffer to visit.
 (setq initial-buffer-choice
@@ -40,78 +63,79 @@
 
 ;; Evil.
 
-;; Modes for which <leader> works in Emacs state.
-(setq evil-leader/no-prefix-mode-rx
-      '("magit-.*-mode" "dired-mode" "gist-list-mode"))
-
-(global-evil-leader-mode)
-(evil-mode 1)
-
-;; Use Emacs keybindings when in insert mode.
-(setcdr evil-insert-state-map nil)
-(define-key evil-insert-state-map [escape] 'evil-normal-state)
-
-(define-key evil-insert-state-map (kbd "<RET>") 'newline-and-indent)
-
-;; Evil mode keys.
-(evil-leader/set-leader "<SPC>")
-
-(define-key evil-normal-state-map (kbd "-") 'dired-jump)
-(define-key evil-normal-state-map (kbd "C-a") 'back-to-indentation)
-(define-key evil-normal-state-map (kbd "H") 'back-to-indentation)
-(define-key evil-normal-state-map (kbd "C-e") 'move-end-of-line)
-(define-key evil-normal-state-map (kbd "L") 'move-end-of-line)
-(define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
-(define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
-
-(define-key evil-normal-state-map (kbd "C-o") 'other-window)
-(global-set-key (kbd "C-o") 'other-window)
-
-(define-key evil-visual-state-map (kbd "a") 'align-regexp)
-
-;; Modes that don't use evil.
-(mapcar (lambda (mode) (add-to-list 'evil-emacs-state-modes mode))
-        '(inferior-emacs-lisp-mode
-          alchemist-iex-mode
-          cider-repl-mode
-          cider-stacktrace-mode
-          git-rebase-mode
-          magit-popup-mode
-          magit-popup-sequence-mode))
-
-(evil-leader/set-key
-  "!" 'shell-command
-  ":" 'eval-expression
-  "o" 'other-window
-  "O" 'other-frame
-  "s |" 'split-window-right
-  "s -" 'split-window-below
-  "b" 'switch-to-buffer
-  "p" 'projectile-switch-project
-  "B" 'evil-buffer)
-
-(evil-leader/set-key-for-mode 'emacs-lisp-mode
-  "e d" 'eval-defun
-  "e b" 'eval-buffer
-  "e s" 'wh/eval-surrounding-sexp
-  "h" 'describe-function)
+(use-package evil
+  :ensure t
+  :init
+  (use-package evil-leader
+    :ensure t
+    :init
+    (setq evil-leader/no-prefix-more-rx '("magit-.*-mode" "dired-mode" "gist-list-mode"))
+    :config
+    (progn
+      (evil-leader/set-leader "<SPC>")
+      (global-evil-leader-mode 1)
+      (evil-leader/set-key
+       "!" 'shell-command
+       ":" 'eval-expression
+       "o" 'other-window
+       "O" 'other-frame
+       "s |" 'split-window-right
+       "s -" 'split-window-below
+       "b" 'switch-to-buffer
+       "p" 'projectile-switch-project
+       "B" 'evil-buffer)
+      (evil-leader/set-key-for-mode 'emacs-lisp-mode
+				    "e d" 'eval-defun
+				    "e b" 'eval-buffer
+				    "e s" 'wh/eval-surrounding-sexp
+				    "h" 'describe-function)))
+  :config
+  (progn
+    (evil-mode 1)
+    ;; Use Emacs keybindings when in insert mode.
+    (setcdr evil-insert-state-map nil)
+    (define-key evil-insert-state-map [escape] 'evil-normal-state)
+    (define-key evil-insert-state-map (kbd "<RET>") 'newline-and-indent)
+    ;; Evil keybindings.
+    (define-key evil-normal-state-map (kbd "-") 'dired-jump)
+    (define-key evil-normal-state-map (kbd "C-a") 'back-to-indentation)
+    (define-key evil-normal-state-map (kbd "H") 'back-to-indentation)
+    (define-key evil-normal-state-map (kbd "C-e") 'move-end-of-line)
+    (define-key evil-normal-state-map (kbd "L") 'move-end-of-line)
+    (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
+    (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
+    (define-key evil-visual-state-map (kbd "a") 'align-regexp)
+    ;; Modes that don't use evil.
+    (setq evil-emacs-state-modes
+          (append
+           evil-emacs-state-modes
+           '(inferior-emacs-lisp-mode
+             alchemist-iex-mode
+             cider-repl-mode
+             cider-stacktrace-mode
+             git-rebase-mode
+             magit-popup-mode
+             magit-popup-sequence-mode)))))
 
 (use-package evil-commentary
+  :ensure t
   :config
   (evil-commentary-mode))
 
-(use-package evil-terminal-cursor-changer)
+(use-package evil-terminal-cursor-changer
+ :ensure t)
 
 (use-package evil-surround
-  :config
-  (progn
-    (global-evil-surround-mode 1)
-    (evil-define-key 'visual evil-surround-mode-map "s" 'evil-surround-region)))
+ :ensure t
+ :config
+ (progn
+   (global-evil-surround-mode 1)
+   (evil-define-key 'visual evil-surround-mode-map "s" 'evil-surround-region)))
 
 (use-package evil-exchange
-  :demand
-  :config
-  (evil-exchange-install))
+ :ensure t
+ :config
+ (evil-exchange-install))
 
 ;; My stuff.
 
@@ -144,20 +168,19 @@
 ;; Misc packages.
 
 (use-package dash
+  :ensure t
   :config
   (dash-enable-font-lock))
 
 (use-package diminish
+  :ensure t
   :config
   (progn
-    (diminish 'undo-tree-mode)
-    (diminish 'evil-commentary-mode)))
-
-(use-package pallet
-  :config
-  (pallet-mode t))
+   (diminish 'undo-tree-mode)
+   (diminish 'evil-commentary-mode)))
 
 (use-package magit
+  :ensure t
   :init
   (setq magit-revert-buffers 'silent)
   :config
@@ -170,14 +193,17 @@
     (define-key magit-status-mode-map (kbd "j") 'magit-section-forward)
     (define-key magit-status-mode-map (kbd "k") 'magit-section-backward)))
 
-(use-package gist)
+(use-package gist
+  :ensure t)
 
 (use-package github-browse-file
+  :ensure t
   :commands github-browse-file
   :init
   (evil-leader/set-key "g b" 'github-browse-file))
 
 (use-package git-messenger
+  :ensure t
   :commands git-messenger:popup-message
   :init
   (setq git-messenger:show-detail t)
@@ -189,6 +215,7 @@
     (define-key git-messenger-map (kbd "RET") 'git-messenger:popup-close)))
 
 (use-package git-timemachine
+  :ensure t
   :commands git-timemachine-toggle
   :init
   (evil-leader/set-key "g t" 'git-timemachine-toggle)
@@ -199,6 +226,7 @@
     (add-hook 'git-timemachine-mode-hook #'evil-normalize-keymaps)))
 
 (use-package helm
+  :ensure t
   :demand t
   :init
   (setq helm-M-x-fuzzy-match t)
@@ -214,6 +242,7 @@
       "F" 'helm-recentf)))
 
 (use-package helm-ag
+  :ensure t
   :commands (helm-do-ag helm-do-ag-project-root)
   :init
   (evil-leader/set-key
@@ -225,13 +254,16 @@
     (add-hook 'helm-ag-mode-hook #'evil-normalize-keymaps)))
 
 (use-package swiper-helm
+  :ensure t
   :bind
   ("C-s" . swiper-helm))
 
 (use-package projectile
+  :ensure t
+  :commands (projectile-find-file projectile-switch-project)
+  :diminish projectile-mode
   :init
   (setq projectile-completion-system 'grizzl)
-  :diminish projectile-mode
   :config
   (progn
     (evil-leader/set-key
@@ -240,6 +272,7 @@
     (projectile-global-mode)))
 
 (use-package guide-key
+  :ensure t
   :init
   (setq guide-key/guide-key-sequence t)
   (setq guide-key/idle-delay 0.4)
@@ -248,6 +281,7 @@
   (guide-key-mode 1))
 
 (use-package popwin
+  :ensure t
   :config
   (progn
     (mapcar (lambda (el) (add-to-list 'popwin:special-display-config el))
@@ -260,20 +294,16 @@
     (global-set-key (kbd "C-l") popwin:keymap)
     (popwin-mode 1)))
 
-(use-package zoom-window
-  :commands 'zoom-window-mode
-  :init
-  (evil-leader/set-key "z" 'zoom-window-zoom))
-
 (use-package company
+  :ensure t
+  :defer 4 ;; load after 4s of idle time
+  :diminish company-mode
   :init
   (setq company-idle-delay 0.1
         company-minimum-prefix-length 2
         company-show-numbers t
         company-dabbrev-downcase nil
         company-dabbrev-ignore-case)
-  :diminish company-mode
-  :defer 4 ;; load after 4s of idle time
   :config
   (progn
     (global-set-key (kbd "C-n") 'company-manual-begin)
@@ -285,6 +315,8 @@
     (global-company-mode t)))
 
 (use-package yasnippet
+  :ensure t
+  :defer 2
   :init
   (setq-default yas-snippet-dirs '("~/.emacs.d/snippets"))
   :diminish yas-minor-mode
@@ -293,25 +325,30 @@
 
 
 (use-package writeroom-mode
+  :ensure t
   :commands writeroom-mode
   :init
   (evil-leader/set-key "m w" 'writeroom-mode)
   :config
   (add-to-list 'writeroom-global-effects 'wh/toggle-tmux-status-bar))
 
-(use-package highlight-sexp
-  :init
-  (add-hook 'clojure-mode-hook 'hl-sexp-mode)
-  (add-hook 'racket-mode-hook 'hl-sexp-mode)
-  (add-hook 'emacs-lisp-mode-hook 'hl-sexp-mode))
-
 (use-package hl-todo
+  :ensure t
   :config
   (global-hl-todo-mode))
+
+;; Correctly load $PATH and $MANPATH on OSX (GUI).
+(use-package exec-path-from-shell
+  :ensure t
+  :if (memq window-system '(mac ns))
+  :config
+  (exec-path-from-shell-initialize))
 
 ;; Modes for programming languages and such.
 
 (use-package web-mode
+  :ensure t
+  :defer 2
   :init
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
@@ -319,14 +356,17 @@
   :mode (("\\.html\\.erb\\'" . web-mode)))
 
 (use-package js
+  ;; built-in
   :init
   (setq js-indent-level 2))
 
 (use-package scss-mode
+  :ensure t
   :init
   (setq css-indent-offset 2))
 
 (use-package erlang
+  :ensure t
   :init
   (setq erlang-indent-level 4))
 
@@ -336,7 +376,7 @@
 
 (use-package alchemist
   :load-path "~/Code/alchemist.el"
-  :demand
+  :demand t
   :diminish alchemist-mode
   :config
   (progn
@@ -360,6 +400,7 @@
       "t p" 'alchemist-mix-test-at-point)))
 
 (use-package markdown-mode
+  :ensure t
   :init
   (setq markdown-open-command "marked")
   :mode (("\\.md\\'" . gfm-mode)
@@ -367,32 +408,41 @@
          ("\\.markdown\\'" . gfm-mode)))
 
 (use-package ruby-mode
+  ;; built-in
   :init
-  ;; Don't insert the coding utf8 comment when saving Ruby files.
+  ;; Don't insert the "coding utf8" comment when saving Ruby files.
   (setq ruby-insert-encoding-magic-comment nil))
 
 (use-package rspec-mode
+  :ensure t
+  :defer t
   :init
   (setq rspec-use-rake-when-possible nil))
 
 (use-package rbenv
+  :ensure t
   :init
   (setq rbenv-installation-dir "/usr/local/Cellar/rbenv/0.4.0"))
 
 (use-package rubocop
+  :ensure t
+  :defer t
   :init
   (add-hook 'ruby-mode-hook 'rubocop-mode))
 
 (use-package projectile-rails
+  :ensure t
+  :defer t
   :config
   (add-hook 'projectile-mode-hook 'projectile-rails-on))
 
-(use-package yaml-mode)
-
-(use-package racket-mode)
+(use-package yaml-mode
+  :ensure t)
 
 (use-package sh-script
+  ;; built-in
   :mode (("\\.zsh\\'" . shell-script-mode)))
+
 
 ;; Miscellaneous stuff.
 
@@ -412,10 +462,6 @@
   (mapcar
    (lambda (m) (add-hook m (lambda () (setq-local show-trailing-whitespace nil))))
    no-trailing-whitespace-modes))
-
-;; Correctly load $PATH and $MANPATH on OSX (GUI).
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
 
 ;; Don't backup/autosave files and don't protect from locks.
 (setq backup-inhibited t)
