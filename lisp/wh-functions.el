204a4cddfd1219b6c8138a62bac58649e2aaeff2
Andrea Leopardi <an.leopardi@gmail.com>
2016-01-10 12:19:17 +0100

    Remove a bunch of rarely-used functions

--- lisp/wh-functions.el
+++ lisp/wh-functions.el
@@ -1,27 +1,5 @@
 ;; Non interactive functions.
 
-(defmacro wh/with-var-set-to (var-and-val &rest body)
-  "Execute `BODY' with a given variable temporarely set to a value.
-
-The value of that variable is restored to the original value after `BODY' is
-executed. `VAR-AND-VAL' is a list with two elements: the variable name (not
-quoted) and its temporary value.
-
-For example:
-
-    (wh/with-var-set-to (alchemist-env \"docs\")
-      (message alchemist-env))
-"
-  (declare (indent defun))
-  (let ((var (car var-and-val))
-        (val (car (cdr var-and-val)))
-        (old-val-symbol (make-symbol "old-val")))
-    `(let ((,old-val-symbol ,var))
-       (set (quote ,var) ,val)
-       (let ((result (progn ,@body)))
-         (set (quote ,var) ,old-val-symbol)
-         result))))
-
 (defun wh/under-tmux-p ()
   "Returns non-nil if the current Emacs instance is running under tmux."
   (and (not (display-graphic-p)) (getenv "TMUX")))
@@ -82,27 +60,6 @@ line and the script will be made executable for the user."
     (save-buffer)
     (shell-script-mode)
     (shell-command (format "chmod u+x %s" path))))
-
-(defun wh/alchemist-generate-docs ()
-  "Generate the documentation for the current Mix project and open it in the
-default browser."
-  (interactive)
-  (wh/with-var-set-to (alchemist-mix-env "docs")
-    (alchemist-mix-execute '("docs")))
-  (shell-command (concat "open " (alchemist-project-root) "doc/index.html")))
-
-(defun wh/alchemist-mix-prompt-for-test-flags (flag)
-  "Prompt for the alchemist test flags to use for the next commands.
-
-This function sets the value of the alchemist-mix-test-default-options global
-variable, so its effect is global and permanent."
-  (interactive "sTest flags: ")
-  (setq alchemist-mix-test-default-options (list flag)))
-
-(defun wh/alchemist-mix-deps-get ()
-  "Fetch the dependencies of the current Mix project with the deps.get task."
-  (interactive)
-  (alchemist-mix-execute '("deps.get")))
 
 (defun wh/projectile-open-todo ()
   "Open TODO.md in the root of the (projectile) project if such file exists."
